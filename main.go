28959e21f167f113cc7d2bd84029c56e0f2dbf93
AN <aliasghernooruddin@outlook.com>
2022-03-25 13:49:32 +0500

    initial skeleton created

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func TestValidity(str string) bool {
+
+}
+
+
+func AverageNumber(str string) float32 {
+
+}
+
+
+func WholeStory(str string) string {
+
+}
+
+
+func StoryStats(str string) (string, string, float32, []string) {
+}
+
+
+func generate(isCorrect bool) string {
+}
+
+func main() {
+	fmt.Println(generate(true))
+	fmt.Println(generate(false))
+}
